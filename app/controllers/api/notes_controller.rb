be0782400fd3d3d0434e6f1f8a3813eb09d42d88
FuChai Mead <jessie.mf.wolff@gmail.com>
2017-12-14 13:57:32 -0700

    sticky notes with rails

---
+++ app/controllers/api/notes_controller.rb
@@ -0,0 +1,43 @@
+class Api::NotesController < ApplicationController
+    before_action :set_note, only: [:show, :update, :destroy]
+   
+     def index
+       render json: Note.all 
+     end
+   
+     def show
+       render json: @note
+     end
+   
+     def create
+       note = Note.create(note_params)
+       if note.save
+         render json: note 
+       else 
+         render json: {errors: note.errors.full_messages.join(',')}, status: 422
+       end 
+     end
+   
+     def update
+       if @note.update(note_params)
+         render json: @note 
+       else 
+         render json: {errors: note.errors.full_messages.join(',')}, status: 422
+       end 
+     end
+   
+     def destroy
+       @note.destroy
+     end
+   
+     private
+     def set_note 
+       @note = Note.find(params[:id])
+     end 
+   
+     def note_params
+       params.require(:note).permit(:name, :body)
+     end 
+  
+   
+end
